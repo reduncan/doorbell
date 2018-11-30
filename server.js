03125c1bf601ceabff10382569d31cd7703badb3
rduncan <rduncan@phpatlanta.com>
2018-11-29 20:56:39 -0500

    adds new files and folders

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+require('./sockets/auth-sockets')(io);
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
+
+db.sequelize.sync({ force: false }).then(function() {
+    server.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
