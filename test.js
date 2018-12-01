9b124fcc11e82aa900d7ea699b33d480e1835c85
weston <westondease@gmail.com>
2018-12-01 10:18:18 -0500

    first test.js

--- test.js
+++ test.js
@@ -10,10 +10,44 @@ chai.use(chaiHttp);
   // set a variable for making http requests.
 let request;
 
-describe('', function() {
-    // clear the test db 
-    beforeEach(function () {
-        request = chai.request(server);
-        return db.sequelize.sync({ force: true });
+describe('GET/routes', function() {
+  // clear the test db 
+  beforeEach(function () {
+      request = chai.request(server);
+      return db.sequelize.sync({ force: true });
   });
-})
+
+  it('should get all authenticated users', function() {
+    db.User.bulkCreate([
+      { username: 'Sally', password: 'test' },
+      { username: 'Lane', password: 'sample' }
+    ])
+    .then(function () {
+
+      //hit the GET('/api/users') endpoint
+      request.get('/api/users').end(function (err, res) {
+        //Save the response
+        let responseStatus = res.status;
+        let responseBody = res.body;
+
+
+        //Write test expectations
+        expect(err).to.be.null;
+
+        expect(responseStatus).to.equal(200);
+
+        expect(responseBody)
+          .to.be.an('array')
+          .that.has.lengthOf(2);
+
+        expect(responseBody[0])
+          .to.be.an('object')
+          .that.includes({ username: 'Sally', password: 'test' });
+
+        expect(responseBody[1])
+          .to.be.an('object')
+          .that.includes({ username: 'Lane', password: 'sample' });
+        done();
+  });
+
+});
