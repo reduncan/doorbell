88b4a35593c22e50e72e93e0f566161c403cbe92
Tri Nguyen <trihuynguyen14@gmail.com>
2018-12-03 01:27:39 -0500

    update server.js and package.json

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-// require('./sockets/auth-sockets')(io);
- require('./routes/api-routes.js')(app);
-// require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
 
-db.sequelize.sync({ force: false }).then(function() {
-    server.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
-}).catch(function(err){
-  console.log("An error occurred")
+
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 })
 
-module.exports = app;
+
