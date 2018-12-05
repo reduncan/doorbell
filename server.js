dbe5e8fc67e805fd9240e1dae1d4dfe4f696060b
Tri Nguyen <trihuynguyen14@gmail.com>
2018-12-04 20:11:16 -0500

    face-api setup

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
+
 const express = require("express");
 const app = express();
 const path = require('path');
-<<<<<<< HEAD
 // const server = require('http').createServer(app);
 // const io = require('socket.io')(server);
-=======
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
->>>>>>> 227ba569ee65149fee76a4f0cee4910a251c2105
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,19 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, '../images')));
 app.use(express.static(path.join(__dirname, '../media')));
 app.use(express.static(path.join(__dirname, './weights')));
 app.use(express.static(path.join(__dirname, './dist')));
 app.use('/uploads', express.static('uploads'));
-=======
-//require('./sockets/auth-sockets')(io);
-require('./routes/api-routes.js')(app);
-//require('./routes/html-routes.js')(app);
-//require('./nodemailer/sendMessage.js')(nodemailer,xoauth2);
->>>>>>> 227ba569ee65149fee76a4f0cee4910a251c2105
 
 
 // require('./sockets/auth-sockets')(io);
