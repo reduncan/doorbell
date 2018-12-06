6372cd98110ea49ab02242ddaf89f39946398321
rduncan <rduncan@phpatlanta.com>
2018-12-06 18:36:34 -0500

    changes photos and threshold

--- dist/face-api.js
+++ dist/face-api.js
@@ -5630,7 +5630,7 @@
 
     var FaceMatcher = /** @class */ (function () {
         function FaceMatcher(inputs, distanceThreshold) {
-            if (distanceThreshold === void 0) { distanceThreshold = 0.45; }
+            if (distanceThreshold === void 0) { distanceThreshold = 0.60; }
             this._distanceThreshold = distanceThreshold;
             var inputArray = Array.isArray(inputs) ? inputs : [inputs];
             if (!inputArray.length) {
