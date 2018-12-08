8c542ce844ad5b2e813f5ce53f2e4b9d76195038
rduncan <rduncan@phpatlanta.com>
2018-12-08 15:01:20 -0500

    makes servo work

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+var five = require("johnny-five");
+var board = new five.Board();
 
 
 const PORT = process.env.PORT || 3000;
@@ -22,14 +24,17 @@ app.use(express.static(path.join(__dirname, './weights')));
 app.use(express.static(path.join(__dirname, './dist')));
 app.use(express.static(path.join(__dirname, './nodemailer')));
 
-
-require('./routes/api-routes.js')(app);
-require('./routes/html-routes.js')(app);
-require('./public/js/auth-receiver.js')
+board.on("ready", function() {
+  var servo = new five.Servo(10);
+  require('./public/js/auth-receiver')(servo);
+  require('./routes/api-routes.js')(app, servo);
+  require('./routes/html-routes.js')(app);
 
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
+})
+
 })
 
