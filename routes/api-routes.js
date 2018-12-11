152854f980329bfe33b42beede5e2ce58418b7aa
rduncan <rduncan@phpatlanta.com>
2018-12-11 13:41:19 -0500

    sets up structure for non-servo app

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,10 @@
-const db = require('../models');
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
 const sendNodeMailer =  require('../nodemailer/sendMessage.js');
 module.exports = function(app, servo) {
 
-  app.get('/api/visitors', function(req, res) {
-    db.Visitor.findAll({}).then(function(dbVisitors) {
-        console.log("get visitor");
-        res.json(dbVisitors);
-    });
-  });
-
-  app.get('/api/owner', function(req, res) {
-    db.Owner.findAll({}).then(function(dbOwner) {
-        console.log("get owner");
-      res.json(dbOwner);
-    });
-  });
-
   app.put('/api/sendNodeMailer',function(req,res){
     sendNodeMailer.sendMessage(nodemailer,xoauth2,req.body.imgSrc);
     res.end();
-  });
-  
-  app.get('/api/servo', function(req,res){
-    servo.max();
-    setTimeout(() => {
-        servo.center();
-    }, 1500);
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-var five = require("johnny-five");
-var board = new five.Board();
-
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
@@ -17,23 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, './images')));
 app.use(express.static(path.join(__dirname, './weights')));
 app.use(express.static(path.join(__dirname, './dist')));
 app.use(express.static(path.join(__dirname, './nodemailer')));
 
-board.on("ready", function () {
-  var servo = new five.Servo(10);
-  require('./public/js/auth-receiver')(servo);
-  require('./routes/api-routes.js')(app, servo);
-  require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
-  db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-      console.log("App listening on PORT " + PORT);
-    });
-  })
-
-})
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
