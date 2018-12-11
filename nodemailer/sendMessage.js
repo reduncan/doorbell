caff2e1d2dcf9c3dfc3ed879642e42d47eb7215d
sandy8111112004 <mingshiuantsai1992@gmail.com>
2018-12-10 21:31:59 -0500

    add comments

--- nodemailer/sendMessage.js
+++ nodemailer/sendMessage.js
@@ -2,47 +2,51 @@
 //Verizon: number@vzwpix.com
 //T-Mobile: number@tmomail.net
 
+/**
+ * @argument nodemailer xauth2 imgURL
+ * @description to send the  visitor's image to owner's cell phone
+ */
+
 require('dotenv').config();
 
 module.exports = {
-  sendMessage: function (nodemailer, xoauth2,imgURL) {
-  let transporter = nodemailer.createTransport(
-    {
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      secure: 'true',
-      port: '465',
-      auth: {
-        type: 'OAuth2',
-        user: `${process.env.emailAdd}`,
-        clientId: `${process.env.clientId}`,
-        clientSecret: `${process.env.clientSecret}`,
-        refreshToken: `${process.env.refreshToken}`
+  sendMessage: function (nodemailer, xoauth2, imgURL) {
+    let transporter = nodemailer.createTransport(
+      {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        secure: 'true',
+        port: '465',
+        auth: {
+          type: 'OAuth2',
+          user: `${process.env.emailAdd}`,
+          clientId: `${process.env.clientId}`,
+          clientSecret: `${process.env.clientSecret}`,
+          refreshToken: `${process.env.refreshToken}`
+        }
       }
-    }
-  );
+    );
 
-  let mailOptions = {
-    from: `${process.env.emailAdd}`,
-    to: `${process.env.phoneNum}`,
-    subject: 'Facebell: You have a visitor!!',
-    text: "Replay to the message if you approve the visitor.",
-    
-    attachments: [{
-      filename: 'visitor',
-      path: `${imgURL}`
-    }]
-  };
+    let mailOptions = {
+      from: `${process.env.emailAdd}`,
+      to: `${process.env.phoneNum}`,
+      subject: 'Facebell: You have a visitor!!',
+      text: "Replay to the message if you approve the visitor.",
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-    transporter.close();
-  });
- 
-  
-}
+      attachments: [{
+        filename: 'visitor',
+        path: `${imgURL}`
+      }]
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+      transporter.close();
+    });
+
+  }
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,28 @@
 const db = require('../models');
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
-const sendNodeMailer =  require('../nodemailer/sendMessage.js');
-module.exports = function(app) {
+const sendNodeMailer = require('../nodemailer/sendMessage.js');
+module.exports = function (app) {
 
-  app.get('/api/visitors', function(req, res) {
-    db.Visitor.findAll({}).then(function(dbVisitors) {
-        console.log("get visitor");
-        res.json(dbVisitors);
+  app.get('/api/visitors', function (req, res) {
+    db.Visitor.findAll({}).then(function (dbVisitors) {
+      console.log("get visitor");
+      res.json(dbVisitors);
     });
   });
 
-  app.get('/api/owner', function(req, res) {
-    db.Owner.findAll({}).then(function(dbOwner) {
-        console.log("get owner");
+  app.get('/api/owner', function (req, res) {
+    db.Owner.findAll({}).then(function (dbOwner) {
+      console.log("get owner");
       res.json(dbOwner);
     });
   });
-  app.put('/api/sendNodeMailer',function(req,res){
-    sendNodeMailer.sendMessage(nodemailer,xoauth2,req.body.imgSrc);
+
+/**
+ * @description to call the function exported in sendMessage.js 
+ */
+  app.put('/api/sendNodeMailer', function (req, res) {
+    sendNodeMailer.sendMessage(nodemailer, xoauth2, req.body.imgSrc);
     res.end();
   });
 };
