5904a589c108325175c5464dd3f7b58f4e9ea64e
rduncan <rduncan@phpatlanta.com>
2018-12-11 14:01:39 -0500

    changes html routes for heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const publicDir = path.join(__dirname, '../public');
 
-
 module.exports = function(app) {
-    app.get('/', (req, res) => res.redirect('/face_bell'));
-    app.get('/face_bell', (req, res) => res.sendFile(path.join(publicDir, './html/index.html')));
+    app.get('*', (req, res) => res.sendFile(path.join(publicDir, './html/index.html')));
 }
-
-
